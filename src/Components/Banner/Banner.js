42d60f2d65d871954c295e1a2f67b128ebffe8d6
elamahpla <theamazingness@gmail.com>
2018-06-18 15:17:03 -0500

    Here we go

--- src/Components/Banner/Banner.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Banner = () => (
-  <div className="Banner">
-    Sweetest Southern Creations
-  </div>
-);
-
-export default Banner;
--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import SSC from './containers/SSC';
-import './Styles/styles.scss';
-import 'normalize.css';
-
-ReactDOM.render(<SSC />, document.getElementById('app'));
-
-module.hot.accept();
