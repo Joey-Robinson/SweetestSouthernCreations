64371305b782d4e8016930445bd19c51698b329f
elamahpla <theamazingness@gmail.com>
2018-06-18 15:32:09 -0500

    Updating Todo.js

---
+++ Todo.js
@@ -0,0 +1,6 @@
+// Start the actual layout: Set the grid up.
+// Design the menu: Clean it up, make it look better with hover effects
+// Add more Routes: Create and add Components for routing and display
+  // Work, Requests, anything else
+// Look into any external libraries needed: Material-UI would be on of the only ones
+// -: ~
