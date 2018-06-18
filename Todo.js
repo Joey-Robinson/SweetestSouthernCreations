90d1b61289bb839b497a339751c80270c03828e0
elamahpla <theamazingness@gmail.com>
2018-06-18 15:36:10 -0500

    Updating Todo.js. Adding title in index.html. Adding Requests & Portfolio components

--- Todo.js
+++ Todo.js
@@ -1,6 +1,8 @@
 // Start the actual layout: Set the grid up.
+// Add the Modal: Take modal from CSSGridStuf and import it here
 // Design the menu: Clean it up, make it look better with hover effects
 // Add more Routes: Create and add Components for routing and display
   // Work, Requests, anything else
 // Look into any external libraries needed: Material-UI would be on of the only ones
 // -: ~
+// Add FavIcon: Request something for FavIcon so it can be added (Do it yourself)
