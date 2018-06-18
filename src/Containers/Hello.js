02b9252b9f6be41b2773759753560908c3962f15
elamahpla <theamazingness@gmail.com>
2018-06-18 15:26:59 -0500

    Had to Copy the internals of LeanPlateRoutes, forgot src

---
+++ src/Containers/Hello.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import CollapsingSidebar from './CollapsingSidebar/CollapsingSidebar';
+import Routes from '../Routes/Routes';
+
+class Hello extends Component {
+  render() {
+    return (
+      <div>
+        <Routes />
+      </div>
+    );
+  }
+};
+
+export default Hello;
