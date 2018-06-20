1192a12bcb9a9297a681a4e14d1d2c3a97be5730
elamahpla <theamazingness@gmail.com>
2018-06-20 14:41:46 -0500

    Maybe too many orbs on About. Will find out later

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -4,7 +4,6 @@ import Footer from '../Layout/Footer/Footer';
 
 const About = () => (
   <section className="About">
-    {/* <Header /> */}
       <div className="About-Dummy">
         <div className="About-Dummy-One">
           <h1>Test</h1>
