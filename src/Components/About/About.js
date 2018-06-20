7c423977677cf99846a462a891914c43dee7d521
elamahpla <theamazingness@gmail.com>
2018-06-20 13:09:11 -0500

    Changing about a bit more. I'm trying to figure the best way to display the information I want without being heavy-handed. Pretty damn difficult.

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import Header from '../Layout/Header/Header';
 import Footer from '../Layout/Footer/Footer';
-import InformationToggle from '../../Containers/InformationToggle/InformationToggle';
 
 const About = () => (
   <section className="About">
-    <Header />
-  <div className="About-Dummy">test</div>
+    {/* <Header /> */}
+      <div className="About-Dummy">
+        <div className="About-Dummy-One">
+          <h1>Test</h1>
+        Lorem ipsum dolor sit amet.
+        </div>
+        <div className="About-Dummy-Two">
+          <h1>Test</h1>
+          Lorem ipsum dolor sit amet.
+        </div>
+        <div className="About-Dummy-Three">
+          <h1>Test</h1>
+          Lorem ipsum dolor sit amet.
+        </div>
+        <div className="About-Dummy-Four">
+          <h1>Test</h1>
+          Lorem ipsum dolor sit amet.
+        </div>
+      </div>
   </section>
 );
 
