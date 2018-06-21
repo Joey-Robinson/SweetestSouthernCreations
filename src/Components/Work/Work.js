31fc615dbb781703949646cd1eb12743a447f896
elamahpla <theamazingness@gmail.com>
2018-06-21 18:59:43 -0500

    I am not a very smart man

--- src/Components/Work/Work.js
+++ src/Components/Work/Work.js
@@ -1,8 +1,29 @@
 import React from 'react';
 
+// const images = ['http://via.placeholder.com/350x150', 'http://via.placeholder.com/350x150', 'http://via.placeholder.com/350x150'];
+
+const images = [
+  'https://placeimg.com/250/250/any',
+  'https://placeimg.com/250/250/any',
+  'https://placeimg.com/250/250/any'
+];
+
 const Work = () => (
   <div className="Work">
     From Work
+    <div>
+      <ul className="Work-UL">
+        {images.map((image, index) => {
+          return <li>
+            <img
+              className="Work-Images" 
+              key={index} 
+              src={image} 
+            />
+          </li>
+        })}
+      </ul>
+    </div>
   </div>
 );
 
