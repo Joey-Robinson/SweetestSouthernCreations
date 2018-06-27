d552754d4874064e45c27780172e778d034d2cec
elamahpla <theamazingness@gmail.com>
2018-06-27 10:02:04 -0500

    Changing some mixins for About.js. Since I don't have access to my email with the content that's supposed to go there, I'm going to do media queries

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,36 +1,34 @@
 import React from 'react';
-import Header from '../Layout/Header/Header';
 import Footer from '../Layout/Footer/Footer';
 
 const About = () => (
   <section className="About">
-    {/* <Header /> */}
-      <div className="About-Dummy">
-        <div className="About-Dummy-Two">
-          <h1>Test</h1>
-            <p>Lorem ipsum dolor sit amet.
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente, maxime!
-            </p>
-        </div>
-        <div className="About-Dummy-Three">
-          <h1>Test</h1>
-            <p>Lorem ipsum dolor sit amet.
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente, maxime!
-            </p>
-        </div>
-        <div className="About-Dummy-Six">
-          <h1>Test</h1>
-            <p>Lorem ipsum dolor sit amet.
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente, maxime!
-            </p>
-        </div>
-        <div className="About-Dummy-Seven">
-          <h1>Test</h1>
-            <p>Lorem ipsum dolor sit amet.
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente, maxime!
-            </p>
-        </div>
+    <div className="About-Dummy">
+      <div className="About-Dummy-Two">
+        <h1>Who are we?</h1>
+          <p>Lorem ipsum dolor sit amet.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente, maxime!
+          </p>
       </div>
+      <div className="About-Dummy-Three">
+        <h1>What do we do?</h1>
+          <p>Lorem ipsum dolor sit amet.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente, maxime!
+          </p>
+      </div>
+      <div className="About-Dummy-Six">
+        <h1>Test</h1>
+          <p>Lorem ipsum dolor sit amet.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente, maxime!
+          </p>
+      </div>
+      <div className="About-Dummy-Seven">
+        <h1>Test</h1>
+          <p>Lorem ipsum dolor sit amet.
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente, maxime!
+          </p>
+      </div>
+    </div>
     <Footer />
   </section>
 );
