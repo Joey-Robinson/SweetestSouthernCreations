a31ff7824a0eff404f0c8c7a51ebe935931d1917
elamahpla <theamazingness@gmail.com>
2018-09-24 18:15:45 -0500

    Wew, let's go

--- src/index.js
+++ src/index.js
@@ -1,94 +1,114 @@
-const hide = () => {
-  const bubblediv = document.getElementById("bubblediv");
-  const button = document.getElementById('hide');
-  if (button.innerHTML === 'Read') {
-    button.textContent = 'Play';
-  } else {
-    button.textContent = 'Read';
-  }
-  if (bubblediv.style.display === "none") {
-    bubblediv.style.display = "block";
-  } else {
-    bubblediv.style.display = "none";
-  }
-};
+// const openUrl = (url) => {
+//   const newTab = window.open();
+//   newTab.opener = null;
+//   newTab.location = url;
+// }
+const openSlide = document
+  .getElementById("openSlideMenu")
+  .addEventListener("click", () => {
+    const slide = document.getElementById("slide");
+    slide.style.width = "60vw";
+  });
 
-const hidediv = document.getElementById("hide").addEventListener("click", hide);
 
-var bubbles = document.getElementById("bubbles"),
-  screenWidth = window.innerWidth,
-  screenHeight = window.innerHeight,
-  containerWidth = 100,
-  marginWidth = (screenWidth - containerWidth - 100) / 2;
-(totalCircles = 2000), (svgNS = "http://www.w3.org/2000/svg");
+const closeSlide = document
+  .getElementById("closeSlideMenu")
+  .addEventListener("click", () => {
+    const slide = document.getElementById("slide");
+    slide.style.width = "0";
+  });
 
-//Set viewBox for bubble container
-bubbles.setAttributeNS(null, "viewbox", `0 0 ${screenWidth} ${screenHeight}`);
+// const hide = () => {
+//   const bubblediv = document.getElementById("bubblediv");
+//   const button = document.getElementById('hide');
+//   if (button.innerHTML === 'Read') {
+//     button.textContent = 'Play';
+//   } else {
+//     button.textContent = 'Read';
+//   }
+//   if (bubblediv.style.display === "none") {
+//     bubblediv.style.display = "block";
+//   } else {
+//     bubblediv.style.display = "none";
+//   }
+// };
 
-// For total number of circles, create circles
-for (let k = 0; k < totalCircles; k++) {
-  createCircle();
-}
+// const hidediv = document.getElementById("hide").addEventListener("click", hide);
 
-//Fill circle with color
-function createCircle() {
-  var circleXPos = getRandom(1, screenWidth);
-  var circleYPos = getRandom(
-    1,
-    (1 / Math.log(circleXPos)) * 8000 - screenHeight * 0.1
-  );
-  var circleRadius = getRandom(4, 30);
-  var circle = document.createElementNS(svgNS, "circle");
+// var bubbles = document.getElementById("bubbles"),
+//   screenWidth = window.innerWidth,
+//   screenHeight = window.innerHeight,
+//   containerWidth = 100,
+//   marginWidth = (screenWidth - containerWidth - 100) / 2;
+// (totalCircles = 2000), (svgNS = "http://www.w3.org/2000/svg");
 
-  var rectArea = screenWidth * screenHeight;
-  // var ellipseArea = (screenWidth - 100) * (screenHeight - 100) * Math.PI;
-  // var remainingArea = rectArea - ellipseArea / 4;
+// //Set viewBox for bubble container
+// bubbles.setAttributeNS(null, "viewbox", `0 0 ${screenWidth} ${screenHeight}`);
 
-  console.log(rectArea);
+// // For total number of circles, create circles
+// for (let k = 0; k < totalCircles; k++) {
+//   createCircle();
+// }
 
-  circle.setAttributeNS(null, "cx", circleXPos);
-  circle.setAttributeNS(null, "cy", circleYPos);
-  circle.setAttributeNS(null, "r", circleRadius);
+// //Fill circle with color
+// function createCircle() {
+//   var circleXPos = getRandom(1, screenWidth);
+//   var circleYPos = getRandom(
+//     1,
+//     (1 / Math.log(circleXPos)) * 8000 - screenHeight * 0.1
+//   );
+//   var circleRadius = getRandom(4, 30);
+//   var circle = document.createElementNS(svgNS, "circle");
 
-  bubbles.appendChild(circle);
-}
+//   var rectArea = screenWidth * screenHeight;
+//   // var ellipseArea = (screenWidth - 100) * (screenHeight - 100) * Math.PI;
+//   // var remainingArea = rectArea - ellipseArea / 4;
 
-// Get Random Integer
-function getRandom(min, max) {
-  return min + Math.floor(Math.random() * (max - min + 1));
-}
+//   console.log(rectArea);
 
-var colors = [
-  "#a7c554",
-  "#bb523d",
-  "#cc7926",
-  "#da21b1",
-  "#4c528a",
-  "#be1ea4",
-  "#20079b",
-  "#7490a7",
-  "#770e31",
-  "#a52a2a",
-  "#663399",
-  "#8b4513",
-  "#ffc0cb",
-  "#b0e0e6",
-  "#d87093"
-];
+//   circle.setAttributeNS(null, "cx", circleXPos);
+//   circle.setAttributeNS(null, "cy", circleYPos);
+//   circle.setAttributeNS(null, "r", circleRadius);
 
-function fillColor(e) {
-  e.setAttribute(
-    "style",
-    "fill:" + colors[Math.floor(Math.random() * colors.length)]
-  );
-}
+//   bubbles.appendChild(circle);
+// }
 
-function bubbleHover() {
-  for (var e = document.querySelectorAll("circle"), t = 0; t < e.length; t++) {
-    e[t].addEventListener("mouseover", function () {
-      fillColor(this);
-    });
-  }
-}
+// // Get Random Integer
+// function getRandom(min, max) {
+//   return min + Math.floor(Math.random() * (max - min + 1));
+// }
 
-bubbleHover();
+// var colors = [
+//   "#a7c554",
+//   "#bb523d",
+//   "#cc7926",
+//   "#da21b1",
+//   "#4c528a",
+//   "#be1ea4",
+//   "#20079b",
+//   "#7490a7",
+//   "#770e31",
+//   "#a52a2a",
+//   "#663399",
+//   "#8b4513",
+//   "#ffc0cb",
+//   "#b0e0e6",
+//   "#d87093"
+// ];
+
+// function fillColor(e) {
+//   e.setAttribute(
+//     "style",
+//     "fill:" + colors[Math.floor(Math.random() * colors.length)]
+//   );
+// }
+
+// function bubbleHover() {
+//   for (var e = document.querySelectorAll("circle"), t = 0; t < e.length; t++) {
+//     e[t].addEventListener("mouseover", function () {
+//       fillColor(this);
+//     });
+//   }
+// }
+
+// bubbleHover();
