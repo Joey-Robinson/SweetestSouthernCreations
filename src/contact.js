ca90e0b3c8d7dc5e4178e3c1bbf271a9f11b8c5f
elamahpla <theamazingness@gmail.com>
2018-10-14 13:10:35 -0500

    Cleaning up -banner

--- src/contact.js
+++ src/contact.js
@@ -13,3 +13,17 @@ const closeSlide = document
     const slide = document.getElementById("slide");
     slide.style.width = "0";
   });
+
+const changeFavIcon = img => {
+  let favIcon = document.querySelector('link[rel="shortcut icon"]');
+  if (!favIcon) {
+    favIcon = document.createElement("link");
+    favIcon.setAttribute("rel", "shortcut icon");
+    const head = document.querySelector("head");
+    head.appendChild(favIcon);
+  }
+  favIcon.setAttribute("type", "image/jpg");
+  favIcon.setAttribute("href", img);
+};
+const favImage = "../images/sscfavicon.jpg";
+changeFavIcon(favImage);
--- src/creations.js
+++ src/creations.js
@@ -284,6 +284,20 @@ const closeSlide = document
     slide.style.width = "0";
   });
 
+
+const changeFavIcon = img => {
+  let favIcon = document.querySelector('link[rel="shortcut icon"]');
+  if (!favIcon) {
+    favIcon = document.createElement("link");
+    favIcon.setAttribute("rel", "shortcut icon");
+    const head = document.querySelector("head");
+    head.appendChild(favIcon);
+  }
+  favIcon.setAttribute("type", "image/jpg");
+  favIcon.setAttribute("href", img);
+};
+const favImage = "../images/sscfavicon.jpg";
+changeFavIcon(favImage);
 // const awesome = document.getElementById("awesome");
 // const awesomeThings = ["one", "two", "three"];
 
